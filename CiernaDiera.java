da412bcf136696504961c67a41f89fc1dba38ea0
TimotejR <timotej.ruzicka@gmail.com>
2025-03-24 19:00:47 +0100

    fix uloha 3 by saveing interupting object in list insted of single var

--- CiernaDiera.java
+++ CiernaDiera.java
@@ -1,25 +1,25 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.List;
+import java.util.ArrayList;
 
 public class CiernaDiera extends Hviezda
 {
-    private VesmirnaLod disabled;
+    private List<VesmirnaLod> disabled = new ArrayList<>();
     public CiernaDiera(int hmotnost){
         this.zivotnost = hmotnost;
     }
     @Override
     protected void Koniec(VesmirnaLod lod){
-        if (this.disabled != null){
-            this.disabled.go();
-        }
-        if (lod != null){
+        while (!disabled.isEmpty()){
+            lod = disabled.get(0);
+            disabled.remove(0);
             lod.go();
         }
         
     }
     @Override
     protected void effectLod(VesmirnaLod lod){
-        this.disabled = lod;
-        this.disabled.stop();
+        this.disabled.add(lod);
+        lod.stop();
     }
 }
